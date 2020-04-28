54dfe7fdeba6a2d9c51b903a3c12164e1e67bb18
Rico Picone <ricopicone@gmail.com>
2020-04-28 13:28:40 -0700

    removed modules from package requirements

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'IPython.display', 'tikzplotlib', 'os', 'matplotlib', 'matplotlib.pyplot',  
+    'IPython', 'tikzplotlib', 'os', 'matplotlib', 'matplotlib',  
     # 'requests', 'maya', 'records',
 ]
 
